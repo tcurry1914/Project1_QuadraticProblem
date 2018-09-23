093e192612fe7870159c981bb928c002878c2e7a
Macallister Armstrong <macallister.armstrong@gmail.com>
2018-09-23 19:36:00 -0400

    adding correct_output with diff statement in bash script

--- version control/make_testing.sh
+++ version control/make_testing.sh
@@ -8,4 +8,10 @@ make run 2>&1 | tee -a $LOG_DIR/log
 make 2>&1 | tee -a $LOG_DIR/log
 make run 2>&1 | tee -a $LOG_DIR/log
 echo "output saved to logs/log"
+diff logs/log logs/correct_output
+if [[ $(diff logs/log logs/correct_output) ]]; then
+  echo "Something went wrong: debug in logs/log."
+else
+  echo "Program output matches correct_output."
+fi
 exit
